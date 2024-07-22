3fa2e965e7f18ec5043e4bf350c22b106c8d3194
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-07-22 14:27:56 +0530

    first commit

--- frontend/src/Routess/Routess.js
+++ frontend/src/Routess/Routess.js
@@ -1,36 +1,25 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-
-
-import Header from '../component/header/Header';
-import Navbar from '../component/navbar/Navbar';
-import Home from '../Page/Home/Home';
-import Login from '../Page/Login/Login';
-import Singup from '../Page/singup/Singup';
-
-
-
+import Header from "../component/header/Header";
+import Home from "../Page/Home/Home";
+import Login from "../Page/Login/Login";
+import Singup from "../Page/singup/Singup";
+import Footer from "../component/footer/Footer";
 
 const Routess = () => {
   return (
-        
-        <div>
+    <div>
       <Header />
-      
+
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Singup />} />
-        
       </Routes>
-      <Navbar/>
-      </div>
-   
-       
-      
-    
-  )
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default Routess
+export default Routess;
---
+++ frontend/src/component/footer/Footer.js
@@ -0,0 +1,41 @@
+import React from "react";
+import "./footer.css";
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="first">
+        <h1 className=" font-bold">GO HACKATHON</h1>
+        <p className=" my-4 p-3">
+          Go Hackathon comprises eminent Industry professionals, educators, and
+          students meant to nurture talent among budding tech professionals.
+        </p>
+      </div>
+      <div className="second font-bold"></div>
+      <h1 className=" font-bold">Quick Links</h1>
+      <div className="links">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+          <li>
+            <a href="#">Events</a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h1 className=" font-bold">Contact Us</h1>
+      </div>
+      <div>
+        <h1 className=" font-bold">Subscribe</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
