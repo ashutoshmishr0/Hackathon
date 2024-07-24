ecd8eb464d8db029a1a2872d745f9d9c2ffb22a7
ashutoshmishr0 <122212306+ashutoshmishr0@users.noreply.github.com>
2024-07-24 15:15:38 +0530

    contact

--- frontend/src/Page/Contact/Contect.js
+++ frontend/src/Page/Contact/Contect.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Contect = () => {
+const Contact = () => {
   return (
     <div>
       <section className="text-gray-600 body-font relative">
@@ -31,21 +31,23 @@ const Contect = () => {
                   EMAIL
                 </h2>
                 <a className="text-indigo-500 leading-relaxed">
-                  example@email.com
+                  info@gohaackathon.in
                 </a>
                 <h2 className="title-font font-semibold text-gray-900 tracking-widest text-xs mt-4">
                   PHONE
                 </h2>
-                <p className="leading-relaxed">123-456-7890</p>
+                <p className="leading-relaxed">9910018925</p>
               </div>
             </div>
           </div>
           <div className="lg:w-1/3 md:w-1/2 bg-white flex flex-col md:ml-auto w-full md:py-8 mt-8 md:mt-0">
             <h2 className="text-gray-900 text-lg mb-1 font-medium title-font">
-              Feedback
+              Contact Us
             </h2>
             <p className="leading-relaxed mb-5 text-gray-600">
-              Post-ironic portland shabby chic echo park, banjo fashion axe
+              We're here to help! Whether you have a question about our
+              programs, an inquiry about an trainings, or just want to say
+              hello, feel free to reach out to us using the information below.
             </p>
             <div className="relative mb-4">
               <label for="name" className="leading-7 text-sm text-gray-600">
@@ -80,7 +82,7 @@ const Contect = () => {
               ></textarea>
             </div>
             <button className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
-              Button
+              Submit
             </button>
             <p className="text-xs text-gray-500 mt-3">
               Chicharrones blog helvetica normcore iceland tousled brook viral
@@ -93,4 +95,4 @@ const Contect = () => {
   );
 };
 
-export default Contect;
+export default Contact;
